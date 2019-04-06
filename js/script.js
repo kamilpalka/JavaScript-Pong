71114e369415dc8d2643fd159236d512610a8a21
Kamil Pałka <palkakamil1988@gmail.com>
2019-04-06 19:00:59 +0200

    script.js collisions, ball move, css improve

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1000;
+canvas.width = 700;
 canvas.height = 500;
 
-class Paddel {
+let boardWidth = canvas.width;
+let playerScore = 0;
+let aiScore = 0;
+
+const ballMoves = gameBalls => {
+  gameBalls.forEach(ball => {
+    ball.moveBall(collObjects);
+  });
+};
+
+const changeBoardSize = () => {
+  boardWidth = canvas.width;
+  aiPaddle.posX = canvas.width - 30;
+};
+
+const screenRefresher = () => {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+class Paddle {
   constructor(width, height, posX, posY, color) {
     this.width = width;
     this.height = height;
@@ -26,30 +46,230 @@ class Ball {
     this.middleHeight = size / 2;
     this.speedX = 2;
     this.speedY = 2;
-    this.DirectionX = true;
-    this.DirectionY = true;
+    this.directionX = true;
+    this.directionY = true;
   }
+
+  moveBall = collObject => {
+    let collision = 0;
+    const ballLeft = this.posX;
+    const ballRight = this.posX + this.width;
+    const ballTop = this.posY;
+    const ballBottom = this.posY + this.height;
+
+    if (this.directionX && this.directionY) {
+      for (let i = 0; i < collObject.length; i++) {
+        let objLeft = collObject[i].posX;
+        let objRight = collObject[i].posX + collObject[i].width;
+        let objTop = collObject[i].posY;
+        let objBottom = collObject[i].posY + collObject[i].height;
+
+        if (this === collObject[i]) continue;
+        else if (
+          ((objLeft <= ballLeft && ballLeft <= objRight) ||
+            (objLeft <= ballRight && ballRight <= objRight)) &&
+          ((objTop <= ballTop && ballTop <= objBottom) ||
+            (objTop <= ballBottom && ballBottom <= objBottom))
+        ) {
+          this.directionX != this.directionX;
+          break;
+        }
+
+        if (
+          ballLeft < objRight &&
+          ((objLeft <= ballLeft + this.speedX &&
+            ballLeft + this.speedX <= objRight) ||
+            (objLeft <= ballRight + this.speedX &&
+              ballRight + this.speedX <= objRight)) &&
+          ballTop < objBottom &&
+          ((objTop <= ballTop + this.speedY &&
+            ballTop + this.speedY <= objBottom) ||
+            (objTop <= ballBottom + this.speedY &&
+              ballBottom + this.speedY <= objBottom))
+        ) {
+          collision = 1;
+          break;
+        } else if (ballBottom + this.speedY > canvas.height) {
+          collision = 2;
+          break;
+        } else if (ballRight + this.speedX > canvas.width) {
+          collision = 3;
+          playerScore++;
+          break;
+        }
+      }
+    } else if (this.directionX && !this.directionY) {
+      for (let i = 0; i < collObject.length; i++) {
+        let objLeft = collObject[i].posX;
+        let objRight = collObject[i].posX + collObject[i].width;
+        let objTop = collObject[i].posY;
+        let objBottom = collObject[i].posY + collObject[i].height;
+
+        if (this === collObject[i]) continue;
+        else if (
+          ((objLeft <= ballLeft && ballLeft <= objRight) ||
+            (objLeft <= ballRight && ballRight <= objRight)) &&
+          ((objTop <= ballTop && ballTop <= objBottom) ||
+            (objTop <= ballBottom && ballBottom <= objBottom))
+        ) {
+          this.directionX != this.directionX;
+          break;
+        }
+
+        if (
+          ballLeft < objRight &&
+          ((objLeft <= ballLeft + this.speedX &&
+            ballLeft + this.speedX <= objRight) ||
+            (objLeft <= ballRight + this.speedX &&
+              ballRight + this.speedX <= objRight)) &&
+          (ballBottom > objTop &&
+            ((objTop <= ballTop - this.speedY &&
+              ballTop - this.speedY <= objBottom) ||
+              (objTop <= ballBottom - this.speedY &&
+                ballBottom - this.speedY <= objBottom)))
+        ) {
+          collision = 1;
+          break;
+        } else if (ballTop - this.speedY < 0) {
+          collision = 2;
+          break;
+        } else if (ballRight + this.speedX > canvas.width) {
+          collision = 3;
+          playerScore++;
+          break;
+        }
+      }
+    } else if (!this.directionX && this.directionY) {
+      for (let i = 0; i < collObject.length; i++) {
+        let objLeft = collObject[i].posX;
+        let objRight = collObject[i].posX + collObject[i].width;
+        let objTop = collObject[i].posY;
+        let objBottom = collObject[i].posY + collObject[i].height;
+
+        if (this === collObject[i]) continue;
+        else if (
+          ((objLeft <= ballLeft && ballLeft <= objRight) ||
+            (objLeft <= ballRight && ballRight <= objRight)) &&
+          ((objTop <= ballTop && ballTop <= objBottom) ||
+            (objTop <= ballBottom && ballBottom <= objBottom))
+        ) {
+          this.directionX != this.directionX;
+          break;
+        }
+
+        if (
+          ballRight > objLeft &&
+          ((objLeft <= ballLeft - this.speedX &&
+            ballLeft - this.speedX <= objRight) ||
+            (objLeft <= ballRight - this.speedX &&
+              ballRight - this.speedX <= objRight)) &&
+          ballTop < objBottom &&
+          ((objTop <= ballTop + this.speedY &&
+            ballTop + this.speedY <= objBottom) ||
+            (objTop <= ballBottom + this.speedY &&
+              ballBottom + this.speedY <= objBottom))
+        ) {
+          collision = 1;
+          break;
+        } else if (ballBottom + this.speedY > canvas.height) {
+          collision = 2;
+          break;
+        } else if (ballLeft - this.speedX < 0) {
+          collision = 3;
+          aiScore++;
+          break;
+        }
+      }
+    } else {
+      for (let i = 0; i < collObject.length; i++) {
+        let objLeft = collObject[i].posX;
+        let objRight = collObject[i].posX + collObject[i].width;
+        let objTop = collObject[i].posY;
+        let objBottom = collObject[i].posY + collObject[i].height;
+
+        if (this === collObject[i]) continue;
+        else if (
+          ((objLeft <= ballLeft && ballLeft <= objRight) ||
+            (objLeft <= ballRight && ballRight <= objRight)) &&
+          ((objTop <= ballTop && ballTop <= objBottom) ||
+            (objTop <= ballBottom && ballBottom <= objBottom))
+        ) {
+          this.directionX != this.directionX;
+          break;
+        }
+
+        if (
+          ballRight > objLeft &&
+          ((objLeft <= ballLeft - this.speedX &&
+            ballLeft - this.speedX <= objRight) ||
+            (objLeft <= ballRight - this.speedX &&
+              ballRight - this.speedX <= objRight)) &&
+          (ballBottom > objTop &&
+            ((objTop <= ballTop - this.speedY &&
+              ballTop - this.speedY <= objBottom) ||
+              (objTop <= ballBottom - this.speedY &&
+                ballBottom - this.speedY <= objBottom)))
+        ) {
+          collision = 1;
+          break;
+        } else if (ballTop - this.speedY < 0) {
+          collision = 2;
+          break;
+        } else if (ballLeft - this.speedX < 0) {
+          collision = 3;
+          aiScore++;
+          break;
+        }
+      }
+    }
+    if (collision) {
+      if (Math.round(Math.random()))
+        this.speedX += Math.round(Math.random()) / 8;
+      else this.speedY += Math.round(Math.random()) / 10;
+      if (collision == 1) {
+        this.directionX = !this.directionX;
+        if (Math.round(Math.random())) this.directionY = !this.directionY;
+      } else if (collision == 2) {
+        this.directionY = !this.directionY;
+      } else {
+        //this.restartBall();
+      }
+    } else {
+      if (this.directionX) this.posX += this.speedX;
+      else this.posX -= this.speedX;
+      if (this.directionY) this.posY += this.speedY;
+      else this.posY -= this.speedY;
+    }
+  };
 }
 
-const drawObject = (collisionObjects, context) => {
-  collisionObjects.forEach(collisionObject => {
-    context.fillStyle = collisionObject.color;
+const drawObject = (collObjects, context) => {
+  collObjects.forEach(collObject => {
+    context.fillStyle = collObject.color;
     context.fillRect(
-      collisionObject.posX,
-      collisionObject.posY,
-      collisionObject.width,
-      collisionObject.height
+      collObject.posX,
+      collObject.posY,
+      collObject.width,
+      collObject.height
     );
   });
 };
 
-const collisionObjects = [];
+const collObjects = [];
+const gameBalls = [];
 
-const playerPaddel = new Paddel(20, 120, 10, 50, "blue");
-const aiPaddel = new Paddel(20, 120, canvas.width - 30, 100, "black");
+const playerPaddle = new Paddle(20, 120, 10, 50, "blue");
+const aiPaddle = new Paddle(20, 120, canvas.width - 30, 100, "orange");
 const ball = new Ball(30, canvas.width / 2 - 4, canvas.height / 2 - 4, "green");
 
-collisionObjects.push(playerPaddel, aiPaddel, ball);
-drawObject(collisionObjects, ctx);
+collObjects.push(playerPaddle, aiPaddle, ball);
+gameBalls.push(ball);
 
-console.log(playerPaddel);
+const game = () => {
+  if (boardWidth !== canvas.width) changeBoardSize();
+  screenRefresher();
+  drawObject(collObjects, ctx);
+  ballMoves(gameBalls);
+};
+
+let play = setInterval(game, 1000 / 60);
